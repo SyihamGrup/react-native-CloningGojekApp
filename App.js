1fce8fed82657d251dc33373ac2fbbef2a54e30a
aldyazarya <aldy1612@gmail.com>
2019-05-18 00:11:55 +0700

    search input and promo section

--- App.js
+++ App.js
@@ -7,12 +7,16 @@ export default class App extends Component {
   render() {
     return (
       <View style={{ flex: 1}}>
-        <View style={{flex: 1, backgroundColor: 'pink'}}>
-          <View>
-            <TextInput placeholder="what do you want to eat?" style={{borderWidth: 1, borderColor: '#E8E8E8', borderRadius: 25, height: 40, fontSize: 13, paddingLeft: 45, paddingRight: 20}}/>
-            <Image/>
-          </View>
-          <View></View>
+        <View style={{flex: 1, backgroundColor: 'white'}}>
+            <View style={{marginHorizontal: 17, flexDirection: 'row', paddingTop: 15}}>
+              <View style={{position: 'relative', flex: 1}}>
+                <TextInput placeholder="what do you want to eat?" style={{borderWidth: 1, borderColor: '#E8E8E8', borderRadius: 25, height: 40, fontSize: 13, paddingLeft: 45, paddingRight: 20, backgroundColor: 'white', marginRight: 18}}/>
+                <Image source={require('./icons/search.png')} style={{ position: 'absolute', height:30, width: 30, left: 12, top: 5}}/>
+              </View>
+              <View style={{width: 35, alignItems: 'center', justifyContent: 'center'}}>
+                <Image source={ require('./icons/promo.png')} style={{width: 30, height: 30}}/>
+              </View>
+            </View>
         </View>
         <View style={{height: 54,flexDirection: 'row', backgroundColor: 'white'}}>
           <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
